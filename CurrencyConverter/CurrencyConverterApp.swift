7b07b5ac5e23c097063965f0b1c24d82fdf959f5
Erkut Cetiner <erkutcetiner0@gmail.com>
2023-01-14 15:59:47 +0300

    Api integration done
     simple and lightweight free API service for current and historical foreign exchange rates implemented for decent currency conversion

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Erkut Cetiner on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
